3084b0143a2a612a03b00d77f80a4885729ada14
Ryan Ellis <ryanrellis@hotmail.com>
2021-05-08 20:19:52 -0300

    Remove Home Controller and Privacy Page

--- MaterialRequest/Startup.cs
+++ MaterialRequest/Startup.cs
@@ -41,7 +41,7 @@ namespace MaterialRequest
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Request/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
